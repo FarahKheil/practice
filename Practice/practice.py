071af5a56cd00d6788caab0dfe146ee220c6c182
Farah kheil <farahkheil23@gmail.com>
2025-09-28 10:32:02 +0300

    pushing new folder

---
+++ Practice/practice.py
@@ -0,0 +1,5 @@
+n = 0
+i = ('*')
+while n < 6:
+    print(i*n)
+    n +=1
---
+++ sessions/session 3.py
@@ -0,0 +1,15 @@
+# is_active = True
+# x=0
+# # check if is_active is true before doing some logic:
+# while is_active is True:
+#     print(x)
+#     print("it is active")
+#     if x>3:
+#         is_active=False
+#     else:
+#         x +=1
+
+
+for i in range (2,8,2):
+    print(i)
+
