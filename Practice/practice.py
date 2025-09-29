b4e3af2c04f53c7a82bc9f211102402b4bd4161f
Farah kheil <farahkheil23@gmail.com>
2025-09-30 02:57:21 +0300

    update code

--- Practice/practice.py
+++ Practice/practice.py
@@ -1,5 +1,18 @@
-n = 0
-i = ('*')
-while n < 6:
-    print(i*n)
-    n +=1
+attempts = 3
+username = 'admin'
+password = 1234
+while attempts > 0:
+   user_input1 = str(input("enter your username : "))
+   user_input2 = str(input("enter your password : "))
+   if user_input1 ==str ('admin') and user_input2 == str(1234):
+          user_input3 = int(input('enter  N: '))
+          for i in range(1, user_input3, 2):
+              print(i%user_input3)
+              break
+   else :
+      attempts -=1
+else:
+    print('account locked')
+    
+     
+    
